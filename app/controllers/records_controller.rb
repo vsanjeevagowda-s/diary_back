8caf88ad7a686905d34594690dae743bf54dd8b5
venkatesh <venkatesh@qwinix.com>
2016-12-30 12:55:40 +0530

    exception in records_controller.rb added

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,12 +1,72 @@
 class RecordsController < ApplicationController
 	before_action :authenticate_user_using_token
 	def create
-		current_user.records << Record.new(record_params)
-		@record = current_user.records.last
-		render json:{
+		begin
+			current_user.records << Record.new(record_params)
+			@record = current_user.records.last
+			render json:{
 				message: "success",
 				data: @record
 			}
+		rescue
+			render json:{
+				message: "failed to create record",
+				status: 201
+			}
+		end
+	end
+
+
+	def edit
+		begin
+			@record = Record.find(params[:id])
+			render json:{
+				data: @record
+			}
+		rescue
+			render json:{
+				message: "record not found",
+				status: 201
+			}
+		end
+	end
+
+	def index
+		@records = current_user.records
+		render json:{
+			data: @records
+		}
+	end
+
+	def update
+		begin
+			@record = Record.find(params[:id])
+			@record.update_attributes(record_params)
+			render json:{
+				data: @record
+			}
+		rescue
+			render json:{
+				message: "failed to update/record not found",
+				status: 201
+			}
+		end
+	end
+
+	def destroy
+		begin
+			@record = Record.find(params[:id])  
+			@record.delete
+			render json:{
+				message: "success",
+				status: 200
+			}
+		rescue
+			render json:{
+				message: "record not found",
+				status: 201
+			}
+		end
 	end
 
 	private
